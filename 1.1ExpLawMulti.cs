ff6854660e85abeeaed79fafc36bfa664fb87d4c
CrateSE <106409369+CrateSE@users.noreply.github.com>
2022-08-11 12:32:16 -0400

    Simply and Revamp
    A change from trying to complete the entirety of Binomial Theorem and Pascals Triangle. Moving through curriculum will be easier under these circumstances as we are scarce on time.

--- 1.1ExpLawMulti.cs
+++ 1.1ExpLawMulti.cs
@@ -1,47 +1,29 @@
-﻿//ExLawForMulti just to do calculations and return the answers.
-static void Splicer(int numbas, string term)
+﻿using System;
+
+namespace ExpLawMulti
 {
 
-    for (int counter = 0; counter < numbas; counter++)
+    public static void Main(string[] args)
     {
-        //Term divider. Seperates each character into placements of an array.
-        char[] ch = new char[term.Length];
-        //This for-loop splices each character into an addressable variable.
-        for (int i = 0; i < term.Length; i++)
+
+        static void ExpLawMulti()
         {
-            ch[i] = term[i];
-            //Breaks terms to exponent and creates addressable exponential number.
-            if (ch[i] == '^')
-            {
-                int realnum = Convert.ToInt32(ch[i - 2]);
-                int exponent = Convert.ToInt32(ch[i + 1]);
-                char charactervar = Convert.ToChar(ch[i - 1]);
-                Console.WriteLine(realnum);
-                Console.WriteLine(exponent);
-                Console.WriteLine(charactervar);
-            }
+            Console.WriteLine("What is your base?: ");
+            double var = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("What is the first exponent you'd like to add?: ");
+            double exp1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("What is the second exponent you'd like to add?: ");
+            double exp2 = Convert.ToDouble(Console.ReadLine());
+
+            double finexp = exp1 + exp2;
+
+            double finans = Math.Pow(var, finexp);
+
+            Console.Write("The result is: {0}^({1} + {2}) = {3}" + '\n' + "{0}^{3} = {4}", var, exp1, exp2, finexp, finans);
         }
+        ExpLawMulti();
+
     }
-
 }
-static void calculations(int realnum, int exponent, int charactervar)
-{
-
-}
-static void Main()
-{
-    string? term = "";
-    Console.WriteLine("How many terms would you like?: ");
-    int numbas = Convert.ToInt32(Console.ReadLine());
-
-    for (int x = 0; x < numbas; x++)
-    {
-        Console.WriteLine("What is your current term?: ");
-        term = Console.ReadLine();
-    }
-
-
-    Splicer(numbas, term);
-
-}
-Main();
